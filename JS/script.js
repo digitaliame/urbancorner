56e66f028f37d4a77b0b44e723d6191ef412db93
DeividZavala <crusedmoss@gmail.com>
2016-04-28 13:43:02 -0500

    cambios a cards y efectos de menu

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,16 @@
-$(".button-collapse").sideNav();
-$('.slider').slider({
-	full_width: true,
-	indicators:false,
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+	$('.slider').slider({
+		full_width: true,
+		indicators:false,
+	});
+	$('.modal-trigger').leanModal();
+	$('.scrollspy').scrollSpy();
+
+	$('#juice').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('deactivated');
+		$('#juice-card').addClass('activated');
+	}));
 });
-$('.modal-trigger').leanModal();
-$('.scrollspy').scrollSpy();
