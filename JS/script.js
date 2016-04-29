6fc1a41cec583f0e9d9e3fe914518025e7eeb393
DeividZavala <crusedmoss@gmail.com>
2016-04-29 08:40:55 -0500

    se agrego toda la funcion de menu

--- JS/script.js
+++ JS/script.js
@@ -12,5 +12,152 @@ $(document).ready(function(){
 		$('#deli-card').addClass('deactivated');
 		$('#juice-card').removeClass('deactivated');
 		$('#juice-card').addClass('activated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
 	}));
+
+	$('#deli').on('click',(function (){
+		$('#deli-card').removeClass('deactivated');
+		$('#deli-card').addClass('activated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
+	}));
+
+	$('#ensaladas').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('deactivated');
+		$('#ensaladas-card').addClass('activated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
+	}));
+
+	$('#crepas').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('deactivated');
+		$('#crepas-card').addClass('activated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
+	}));
+
+	$('#smth').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('deactivated');
+		$('#smoothi-card').addClass('activated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
+	}));
+
+	$('#te').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('deactivated');
+		$('#te-card').addClass('activated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
+	}));
+
+	$('#cafe').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('deactivated');
+		$('#cafe-card').addClass('activated');
+		$('#frappe-card').removeClass('activated');
+		$('#frappe-card').addClass('deactivated');
+	}));
+
+	$('#frappe').on('click',(function (){
+		$('#deli-card').removeClass('activated');
+		$('#deli-card').addClass('deactivated');
+		$('#juice-card').removeClass('activated');
+		$('#juice-card').addClass('deactivated');
+		$('#ensaladas-card').removeClass('activated');
+		$('#ensaladas-card').addClass('deactivated');
+		$('#crepas-card').removeClass('activated');
+		$('#crepas-card').addClass('deactivated');
+		$('#smoothi-card').removeClass('activated');
+		$('#smoothi-card').addClass('deactivated');
+		$('#te-card').removeClass('activated');
+		$('#te-card').addClass('deactivated');
+		$('#cafe-card').removeClass('activated');
+		$('#cafe-card').addClass('deactivated');
+		$('#frappe-card').removeClass('deactivated');
+		$('#frappe-card').addClass('activated');
+	}));
+
+
 });
